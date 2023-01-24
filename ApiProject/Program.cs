9f3fc7301589c6aaef2eb593b03a4873c26a43e3
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-01-24 18:45:14 +0100

    Add basic autentication and authorization
    added authentication via bearer token created in AuthenticationControllers, auth checking for username and password are demo code not for production use

--- ApiProject/Program.cs
+++ ApiProject/Program.cs
@@ -1,10 +1,14 @@
+using ApiProject.StartupServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization(opts => AuthServices.AddAuthorizationOptions(opts));
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(opts => AuthServices.AddJwtBearerOptions(builder, opts));
 
 var app = builder.Build();
 
@@ -17,6 +21,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
