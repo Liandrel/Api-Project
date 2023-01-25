f3a0f727cd347a933c05b93343876ebfe0589232
Liandrel <109763767+Liandrel@users.noreply.github.com>
2023-01-25 19:13:30 +0100

    Add logging
    added logging for each endpoint of controller, info logs for beggining of the call and Error log for exception caught

--- ApiProject/Controllers/TodosController.cs
+++ ApiProject/Controllers/TodosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using TodoLibrary.Data;
 using TodoLibrary.DataAccess;
 using TodoLibrary.Models;
@@ -11,11 +12,12 @@ namespace ApiProject.Controllers;
 public class TodosController : ControllerBase
 {
     private readonly ITodoData _todoData;
+    private readonly ILogger<TodosController> _logger;
 
-    public TodosController(ITodoData todoData)
+    public TodosController(ITodoData todoData, ILogger<TodosController> logger)
     {
         _todoData = todoData;
-        
+        _logger = logger;
     }
 
     private int GetUserId()
@@ -28,56 +30,128 @@ public class TodosController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<List<TodoModel>>> Get()
     {
-        var output = await _todoData.GetAllAssigned(GetUserId());
+        int userId = GetUserId();
 
-        return StatusCode(200,output);
+        _logger.LogInformation("GET: {ApiPath} by {UserId}", "api/Todos", userId);
+        try
+        {
+            var output = await _todoData.GetAllAssigned(userId);
+
+            return StatusCode(200, output);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "GET call to {ApiPath} by {UserId} has failed.", "api/Todos", userId);
+
+            return StatusCode(400);
+        }
     }
 
     // GET api/Todos/5
     [HttpGet("{todoId}")]
     public async Task<ActionResult<TodoModel>> Get(int todoId)
     {
-        var output = await _todoData.GetOneAssigned(GetUserId(), todoId);
+        int userId = GetUserId();
 
-        return StatusCode(200, output);
+        _logger.LogInformation("GET: {ApiPath} by {UserId}. Todo Id was {TodoId}", $"api/Todos/Id", userId, todoId);
+        try
+        {
+            var output = await _todoData.GetOneAssigned(userId, todoId);
+
+            return StatusCode(200, output);
+        }
+        catch (Exception ex)
+        {
+
+            _logger.LogError(ex, "GET call {ApiPath} by {UserId} has failed. Todo Id was {TodoId}", $"api/Todos/Id", userId, todoId);
+
+            return StatusCode(400);
+        }
     }
 
     // POST api/Todos
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] string task)
     {
-        var output = await _todoData.CreateTodo(GetUserId(), task);
+        int userId = GetUserId();
 
-        return StatusCode(200,output);
+        _logger.LogInformation("POST: {ApiPath} by {UserId}. Task: {Task}", $"api/Todos", userId, task);
+        try
+        {
+            var output = await _todoData.CreateTodo(userId, task);
+
+            return StatusCode(200, output);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "POST call {ApiPath} by {UserId} has failed. The value was {Task}", $"api/Todos", userId, task);
+
+            return StatusCode(400);
+
+        }
     }
 
     // PUT api/Todos/5
     [HttpPut("{todoId}")]
-    public async Task<IActionResult> Put(int todoId, [FromBody] string value)
+    public async Task<IActionResult> Put(int todoId, [FromBody] string task)
     {
-        await _todoData.UpdateTask(GetUserId(), todoId, value);
+        int userId = GetUserId();
 
-        return StatusCode(200);
+        _logger.LogInformation("PUT: {ApiPath} by {UserId}. Todo Id was {TodoId}. Task: {Task}", $"api/Todos/Id", userId, todoId, task);
+        try
+        {
+            await _todoData.UpdateTask(userId, todoId, task);
+
+            return StatusCode(200);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "PUT call {ApiPath} by {UserId} has failed. Todo Id was {TodoId}. The value was {Task}", $"api/Todos/Id", userId, todoId, task);
+
+            return StatusCode(400);
+
+        }
     }
 
     // PUT api/Todos/5/Complete
     [HttpPut("{todoId}/Complete")]
     public async Task<IActionResult> Complete(int todoId)
     {
-        await _todoData.CompleteTodo(GetUserId(), todoId);
+        int userId = GetUserId();
 
+        _logger.LogInformation("PUT: {ApiPath} by {UserId}. Todo Id was {TodoId}", $"api/Todos/Id/Complete", userId, todoId);
+        try
+        {
+            await _todoData.CompleteTodo(userId, todoId);
 
-        return StatusCode(200);
+            return StatusCode(200);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "PUT call {ApiPath} by {UserId} has failed. Todo Id was {TodoId}", $"api/Todos/Id/Complete", userId, todoId);
+
+            return StatusCode(400);
+        }
     }
 
     // DELETE api/Todos/5
     [HttpDelete("{todoId}")]
     public async Task<IActionResult> Delete(int todoId)
     {
-        await _todoData.DeleteTodo(GetUserId(), todoId);
+        int userId = GetUserId();
 
 
-        return StatusCode(200);
+        try
+        {
+            await _todoData.DeleteTodo(userId, todoId);
+
+            return StatusCode(200);
+        }
+        catch (Exception ex)
+        {
+
+            return StatusCode(400);
+        }
 
     }
 }
